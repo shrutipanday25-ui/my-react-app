5533704d7439fa8d0d8c902a249456130a6aab0e
shrutipanday25-ui <shrutipanday25@navgurukul.org>
2026-09-18 13:41:48 -0500

    Day 1 React assignment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App(){
+  return(
+    <div>
+      <h1>Hello React </h1>
+      <ul> 
+        <li> React</li>
+        <li> Python</li>
+        <li> JavaScript</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
